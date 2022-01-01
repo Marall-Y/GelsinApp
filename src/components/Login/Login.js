e5ca043eebe35644f7bf519eb8bd825efda534b1
Maral <maralyoosefi@gmail.com>
2022-01-02 01:19:34 +0300

    debug login page

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import useStyles from "./Style";
 import {
   Checkbox,
@@ -10,14 +10,60 @@ import {
 } from "@material-ui/core";
 import Button from "@material-ui/core/Button";
 import logo from "../../images/Logo.png";
+import { Redirect } from "react-router-dom";
 import { primary } from "../../StyleGuide/Colors";
+import axios from "../../axios-orders";
+import history from "../../history";
 
 const Login = () => {
   const classes = useStyles();
   const [checked, setChecked] = useState(true);
+  const [data, setData] = useState([]);
+  const [phone, setPhone] = useState("");
+  const [pass, setPass] = useState("");
+  const [user, setUser] = useState("");
+
+  useEffect(async () => {
+    try {
+      const resp = await axios.get("/users");
+      setData(resp.data);
+    } catch (err) {
+      // Handle Error Here
+      console.error(err);
+    }
+  }, []);
 
   const handleChange = (event) => {
     setChecked(event.target.checked);
+  };
+
+  const loginHandler = () => {
+    setPhone("");
+    setPass("");
+    data.map((item) => {
+      if (item.phone === phone) {
+        if (item.password == pass) {
+          setUser(item);
+          window.location.href = "/";
+        } else {
+          return item;
+        }
+      } else {
+        return item;
+      }
+    });
+  };
+
+  useEffect(() => {
+    localStorage.setItem("user", JSON.stringify(user));
+  }, [user]);
+
+  const phoneHandler = (event) => {
+    setPhone(event.target.value);
+  };
+
+  const passHandler = (event) => {
+    setPass(event.target.value);
   };
 
   return (
@@ -40,10 +86,19 @@ const Login = () => {
               alignItems={"center"}
             >
               <Grid item xs={12}>
-                <TextField label="Telefon"></TextField>
+                <TextField
+                  label="Telefon"
+                  value={phone}
+                  onChange={phoneHandler}
+                />
               </Grid>
               <Grid item xs={12}>
-                <TextField label="Şifre" type={"password"}></TextField>
+                <TextField
+                  label="Şifre"
+                  type={"password"}
+                  value={pass}
+                  onChange={passHandler}
+                />
               </Grid>
               <Grid item xs={12}>
                 <FormControlLabel
@@ -59,7 +114,11 @@ const Login = () => {
                 />
               </Grid>
               <Grid item xs={12}>
-                <Button variant="contained" color="primary">
+                <Button
+                  variant="contained"
+                  color="primary"
+                  onClick={loginHandler}
+                >
                   Giriş Yap
                 </Button>
               </Grid>
--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
+import axios from "axios";
 import React from "react";
+import history from "./history";
 import * as ReactDOM from "react-dom";
+
 import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<App history={history} />, document.getElementById("root"));

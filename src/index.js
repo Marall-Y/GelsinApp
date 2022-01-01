0176748eae05b750d9e0fbdff6378b822d35271e
Maral <maralyoosefi@gmail.com>
2022-01-01 21:57:21 +0300

    login page

--- src/index.js
+++ src/index.js
@@ -1,11 +1,5 @@
-import axios from "axios";
 import React from "react";
 import * as ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-
-const baseUrl = axios.create({
-  baseURL: "https://gelsin-99035-default-rtdb.firebaseio.com/",
-});
 
 ReactDOM.render(<App />, document.getElementById("root"));
